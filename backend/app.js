a5497eb4dedf833db455ee7bebb50c19ef333d1f
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-12 13:43:58 +0530

    Create the first api call and parsed the json data

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,27 @@ const app = express();
 
 require('dotenv/config')
 
-const api = process.env.API_URL
-app.get(api+'/', (req, res)=>{
-    res.send("Hello World!");
+const api = process.env.API_URL;
+
+//Middleware 
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res)=>{
+    const product = {
+        id: 1,
+        name: 'Iphone 14',
+        image: 'some-url',
+        price: 55000
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(3000, ()=>{
-    console.log(api)
     console.log("Server is runinng on port http://localhost:3000")
 })
