34bffcba00db79eeb2618ad4ea53161cef9fa13e
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-12 14:24:41 +0530

    Install the mongoose and connected to the backend

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,41 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-require('dotenv/config')
+require("dotenv/config");
 
 const api = process.env.API_URL;
 
-//Middleware 
+//Middleware
 app.use(express.json());
+app.use(morgan("tiny"));
 
-app.get(`${api}/products`, (req, res)=>{
-    const product = {
-        id: 1,
-        name: 'Iphone 14',
-        image: 'some-url',
-        price: 55000
-    }
-    res.send(product);
-})
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "Iphone 14",
+    image: "some-url",
+    price: 55000,
+  };
+  res.send(product);
+});
 
-app.post(`${api}/products`, (req, res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
-})
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+});
 
-app.listen(3000, ()=>{
-    console.log("Server is runinng on port http://localhost:3000")
-})
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Database connection is ready");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+app.listen(3000, () => {
+  console.log("Server is runinng on port http://localhost:3000");
+});
