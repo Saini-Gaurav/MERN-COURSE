f36994fa4b2ce40e5a83873a16fd31b13c4f4f7c
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-13 13:46:50 +0530

    Add the product model and Product Schema

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,12 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    name: {type: String, required: true},
-    image: String,
-    countInStock: Number
-})
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  richDescription: { type: String, default: "" },
+  image: { type: String, default: "" },
+  images: [{ type: String }],
+  brand: { type: String, default: "" },
+  price: { type: Number, default: 0 },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: { type: Number, required: true, min: 0, max: 255 },
+  rating: { type: Number, default: 0 },
+  numReviews: { type: Number, default: 0 },
+  isFeatured: { type: Boolean, default: false },
+  dateCreated: { type: Date, default: Date.now },
+});
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
