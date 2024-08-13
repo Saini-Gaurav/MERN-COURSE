ad6b0fdb7eef66709af5c3dea876b00fb3a86334
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-13 14:50:53 +0530

    Add the category schema and add a category using post request && delete a category using delete request

--- backend/models/category.js
+++ backend/models/category.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const categorySchema = new Schema({
+    name: {type: String, required: true},
+    icon: {type: String},
+    color: {type: String}
 
 });
 
--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,12 +3,44 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const categoryList = await Category.find();
+  const categoryList = await Category.find();
 
-    if(!categoryList){
-        return res.status(500).json({success: false})
+  if (!categoryList) {
+    return res.status(500).json({ success: false });
+  }
+  res.send(categoryList);
+});
+
+router.post("/", async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  category = await category.save();
+
+  if (!category) {
+    return res.status(404).send("Category cannot be created");
+  }
+
+  res.send(category);
+});
+
+router.delete("/:id", async (req, res) => {
+  const categoryId = req.params.id;
+  try {
+    const category = await Category.findByIdAndDelete(categoryId);
+    if (!category) {
+      return res.status(404).send("Category cannot be deleted");
     }
-    res.send(categoryList);
+
+    return res
+      .status(200)
+      .send({ succes: true, message: "Category deleted Successfully" });
+  } catch (err) {
+    return res.status(400).send({ success: false, error: err });
+  }
 });
 
 module.exports = router;
