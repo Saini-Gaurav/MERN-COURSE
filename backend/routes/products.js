fa9c9e8325a6ba5e494e2c18aeb3ba815313d07c
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-14 13:19:18 +0530

    Send a post request to create a new product with updated schema

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,33 +1,56 @@
 const express = require("express");
-const Product = require('../models/products')
+const Product = require("../models/products");
+const { Category } = require("../models/category");
 
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find()
-    if(!productList){
-      return res.status(500).json({success: false})
+  try {
+    const productList = await Product.find();
+    if (!productList) {
+      return res.status(500).json({ success: false });
     }
     res.send(productList);
-  });
-  
-  router.post(`/`, (req, res) => {
-    const product = new Product({
-      name: req.body.name,
-      image: req.body.image,
-      countInStock: req.body.countInStock,
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err.message,
     });
-    product
-      .save()
-      .then((createdProduct) => {
-        res.status(201).json(createdProduct);
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: err,
-          success: false,
-        });
-      });
-  });
-  
-  module.exports = router;
+  }
+});
+
+router.post(`/`, async (req, res) => {
+  try {
+    const category = await Category.findById(req.body.category);
+    if (!category) {
+      return res.status(404).send("Invalid Category");
+    }
+    let product = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      countInStock: req.body.countInStock,
+      isFeatured: req.body.isFeatured,
+    });
+
+    product = await product.save();
+
+    if (!product) {
+      return res.status(500).send("The product cannot be created");
+    }
+    return res.send(product);
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+module.exports = router;
