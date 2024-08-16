4d5182dbe5589c4edfb141c4aefa61b51d7c2749
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-16 10:57:16 +0530

    Send a post request to create a new user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,51 @@
-const { User } = require('../models/user');
-const express = require('express');
+const { User } = require("../models/user");
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', async (req, res)=>{
+router.get("/", async (req, res) => {
+  try {
     const userList = await User.find();
 
-    if(!userList){
-        return res.status(500).json({success: false})
+    if (!userList) {
+      return res.status(500).json({ success: false });
     }
     res.send(userList);
-})
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+
+    user = await user.save();
+
+    if(!user){
+        return res.status(500).send('User cannot be created');
+    }
+    return res.status(200).send(user);
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
