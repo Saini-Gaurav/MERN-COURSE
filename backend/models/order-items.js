578b42f701b78d5be59c9819b934e854c5241a68
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-17 16:58:24 +0530

    Created the Models and Schema for Orders and Order-Item

---
+++ backend/models/order-items.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const orderItemSchema = new Schema({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  }
+});
+
+exports.OrderItem = mongoose.model("OrderItem", orderItemSchema);
