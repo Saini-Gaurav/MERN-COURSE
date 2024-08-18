ec0e5b4dd5ded53d9fd3c3e2c1efdc4bd88446b7
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-18 14:07:31 +0530

    Send a get request to get a single product and also show the details of the order item and category

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -6,7 +6,9 @@ const router = express.Router();
 // Route to get all the orders
 router.get(`/`, async (req, res) => {
   try {
-    const orderList = await Order.find();
+    const orderList = await Order.find()
+      .populate("user", "name")
+      .sort({ dateOrdered: -1 });
 
     if (!orderList) {
       res.status(500).json({ success: false, message: "No order found" });
@@ -20,17 +22,43 @@ router.get(`/`, async (req, res) => {
   }
 });
 
+// Route to get the specific order based on the order id
+router.get("/:id", async (req, res) => {
+  try {
+    const orderId = req.params.id;
+    const order = await Order.findById(orderId)
+      .populate("user", "name")
+      .populate({
+        path: "orderItems",
+        populate: { path: "product", populate: "category" },
+      });
+    if (!order) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Order not found" });
+    }
+    return res.send(order);
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
 // Route to create a order
 router.post("/", async (req, res) => {
-  const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
-    let newOrderItem = new OrderItem({
-      quantity: orderItem.quantity,
-      product: orderItem.product,
-    });
-    newOrderItem = await newOrderItem.save();
-    return newOrderItem._id;
-  }));
-  
+  const orderItemsIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+      newOrderItem = await newOrderItem.save();
+      return newOrderItem._id;
+    })
+  );
+
   const orderItemsIdsResolved = await orderItemsIds;
   try {
     let order = new Order({
